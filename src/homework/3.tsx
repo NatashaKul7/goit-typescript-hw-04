ad5244b0eeff7a8c20194f431a74d4bc8baea9fc
NatashaKul7 <natasha.kulibaba0703@gmail.com>
2023-11-18 18:15:39 +0100

    1task

--- src/homework/3.tsx
+++ src/homework/3.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 
 export function FormComponent() {
-  const [value, setValue] = useState("");
+  const [value, setValue] = useState<string>("");
 
   const handleChange = (event) => {
     setValue(event.target.value);
